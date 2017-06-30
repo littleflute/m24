af66874787b81ebcbe8bb83e51ff0bd18db5ce1a
littleflute <13699175164@sina.cn>
2017-06-30 15:20:02 -0500

    Add files via upload

---
+++ Deep Purple/Machine Head/m.js
@@ -0,0 +1,10 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Highway Star.mp3";		
+f[2]	= "02 Maybe I'm a Leo.mp3";		
+f[3]	= "03 Pictures of Home.mp3";		
+f[4]	= "04 Never Before.mp3";		
+f[5]	= "05 Smoke on the Water.mp3";		
+f[6]	= "06 Lazy.mp3";		
+f[7]	= "07 Space Truckin'.mp3";		
+f[8]	= "08 When a Blind Man Cries.mp3";		
