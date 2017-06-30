d2935cc66f1cd3a8323002fa02d4c77c0cf45bf7
littleflute <13699175164@sina.cn>
2017-06-30 15:20:16 -0500

    Add files via upload

---
+++ Flogging Molly/Drunken Lullabies/m.js
@@ -0,0 +1,14 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Drunken Lullabies.mp3";		
+f[2]	= "02 What's Left of the Flag.mp3";		
+f[3]	= "03 May the Living Be Dead (In Our Wake).mp3";		
+f[4]	= "04 If I Ever Leave This World Alive.mp3";		
+f[5]	= "05 The Kilburn High Road.mp3";		
+f[6]	= "06 Rebels of the Sacred Heart.mp3";		
+f[7]	= "08 Cruel Mistress.mp3";		
+f[8]	= "09 Death Valley Queen.mp3";		
+f[9]	= "10 Another Bag of Bricks.mp3";		
+f[10]	= "11 The Rare Ould Times.mp3";		
+f[11]	= "12 The Son Never Shines (On Closed Doors).mp3";		
+f[12]	= "Swagger.mp3";		
