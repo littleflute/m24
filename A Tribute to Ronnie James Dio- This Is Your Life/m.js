b71bd45e8857a3543add6e4b0668f769174207aa
littleflute <13699175164@sina.cn>
2017-06-30 15:18:45 -0500

    Add files via upload

---
+++ A Tribute to Ronnie James Dio- This Is Your Life/m.js
@@ -0,0 +1,16 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Neon Knights [_].mp3";		
+f[2]	= "02 The Last in Line [_].mp3";		
+f[3]	= "03 The Mob Rules.mp3";		
+f[4]	= "04 Rainbow in the Dark [_].mp3";		
+f[5]	= "05 Straight Through the Heart [_].mp3";		
+f[6]	= "06 Starstruck [_].mp3";		
+f[7]	= "07 The Temple of the King [_].mp3";		
+f[8]	= "08 Egypt (The Chains Are On).mp3";		
+f[9]	= "09 Holy Diver.mp3";		
+f[10]	= "10 Catch the Rainbow [_].mp3";		
+f[11]	= "11 I [_].mp3";		
+f[12]	= "12 Man on the Silver Mountain [_].mp3";		
+f[13]	= "13 Ronnie Rising Medley- A Light in the Black-Tarot Woman-Stargazer-Kill t.mp3";		
+f[14]	= "14 This Is Your Life.mp3";		
